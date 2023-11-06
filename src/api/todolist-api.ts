eb5c1457f85a15cc2fd82f984e9a63c509030f56
Igor Olkhov <smartestbox@gmail.com>
2023-11-07 00:25:37 +0100

    Make types on todolist api

--- src/api/todolist-api.ts
+++ src/api/todolist-api.ts
@@ -7,19 +7,38 @@ const instance = axios.create({
 
 export const todolistAPI = {
     getTodolists() {
-        return instance.get('todo-lists')
+        return instance.get<TodolistType[]>('todo-lists')
             .then(res => res.data)
     },
     createTodolist(title: string) {
-        return instance.post(
+        return instance.post<CreateTodolistResponseType>(
             'todo-lists', {title})
     },
     deleteTodolist(todolistId: string) {
-        return instance.delete(
+        return instance.delete<ResponseType>(
             `todo-lists/${todolistId}`)
     },
     updateTodolist(todolistId: string, title: string) {
-        return instance.put(
+        return instance.put<UpdateTodolistResponseType>(
             `todo-lists/${todolistId}`, {title})
     }
 }
+
+type TodolistType = {
+    id: string
+    title: string
+    addedDate: Date
+    order: number
+}
+
+type ResponseType<D = {}> = {
+    resultCode: number
+    messages: string[]
+    data: D
+}
+
+type UpdateTodolistResponseType = ResponseType & {
+    fieldsErrors: string[]
+}
+
+type CreateTodolistResponseType = ResponseType<{item: TodolistType}>
